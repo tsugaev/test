af1b92784609423b4e7cf89e68ca21e7573866a1
kuduzow <admin@intocode.ru>
2020-11-27 20:59:56 +0300

    изменил дизайн

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          👋🏼Hi from intocode!
+          👋🏼Hello from intocode!🦁
         </p>
       </header>
     </div>
